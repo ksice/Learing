0d54850717bc1246c4a58252f9d8718ec02d75cd
ksice <17670659687@163.com>
2022-03-31 11:17:36 +0800

    1、 自除数

---
+++ src/main/java/leetcode/Leetcode728.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 自除数
+ *
+ * @Version : 1.0
+ * @Creation : 2022/3/31 上午10:43
+ * @Author : ksice_xt
+ */
+public class Leetcode728 {
+
+    public static void main(String[] args) {
+        System.out.println(2 % 10);
+
+    }
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //指定标签，让循环中的循环可以直接跳到这个循环上
+        out:
+        for (int i = left; i <= right; i++) {
+            int cur = i;
+
+            //得出i的个位数例如123得到3
+            while (cur != 0) {
+                int i1 = cur % 10;
+
+                //不能除尽就换下一位
+                if (i1 == 0 || i % i1 != 0) {
+                    continue out;
+                }
+                //然后再除以10，取下一位数字，例如123 变成12在取模得到2，感觉这个方式很牛逼
+                cur /= 10;
+
+            }
+            list.add(i);
+        }
+        return list;
+    }
+}
