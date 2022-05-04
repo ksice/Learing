54b9982c1340564a4b1103ef19aac534c43fecf2
ksice <17670659687@163.com>
2022-05-04 14:15:34 +0800

    1、零钱兑换（经典完全背包算法可以对同一个物品多选）

--- src/main/java/leetcode/may/Leetcode322.java
+++ src/main/java/leetcode/may/Leetcode322.java
@@ -1,5 +1,7 @@
 package leetcode.may;
 
+import com.sun.org.apache.xml.internal.security.Init;
+
 import java.util.Arrays;
 
 /**
@@ -12,24 +14,24 @@ import java.util.Arrays;
 public class Leetcode322 {
 
     public int coinChange(int[] coins, int amount) {
+
         int max = amount + 1;
 
         int[] dp = new int[amount + 1];
-
         Arrays.fill(dp, max);
-
-        dp[0]=0;
+        //第一个dp为0,否则的出下面的dp都是max
+        dp[0] = 0;
+        //完全背包和0-1背包主要就是循环的区别
+        //得出从1开始凑到amount金额的最小硬币数
         for (int i = 1; i <= amount; i++) {
-            //完全背包算法，一个物品可以被使用多次
-            //0-1背包是每个物品只能使用一次
+            //硬币数放在内部循环
             for (int j = 0; j < coins.length; j++) {
+                //硬币数要小于等于amount值
                 if (coins[j] <= i) {
-                    //因为每个硬币都在这重新加一
                     dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
-
         return dp[amount] > amount ? -1 : dp[amount];
     }
 }
