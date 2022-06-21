f1ad265d1300218b9a461343c97444ccf8c37bd2
ksice <17670659687@163.com>
2022-06-21 12:23:57 +0800

    1、判断链表是否有环

--- src/main/java/leetcode/hot100/Leetcode141.java
+++ src/main/java/leetcode/hot100/Leetcode141.java
@@ -12,6 +12,7 @@ import java.util.Set;
  */
 public class Leetcode141 {
     public boolean hasCycle(ListNode head) {
+
         Set<ListNode> set = new HashSet<>();
         while (head != null) {
 
